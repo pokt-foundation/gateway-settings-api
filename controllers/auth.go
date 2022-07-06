3a61e931c870ef1caf254ba39f3158a0dfb0fbc3
crisog <ortega.cpp@gmail.com>
2022-07-06 15:53:24 -0400

    fix: read jwt signing key from env

--- controllers/auth.go
+++ controllers/auth.go
@@ -77,7 +77,7 @@ func Login(c *fiber.Ctx) error {
 	claims["applicationIDs"] = applicationIDs
 	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
-	t, err := token.SignedString([]byte("secret"))
+	t, err := token.SignedString([]byte(configs.EnvJWTSigningKey()))
 	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
